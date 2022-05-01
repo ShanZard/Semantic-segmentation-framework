28c65df8d4e0bb49531e4005126860c663ad648e
ShanZard <87146029+ShanZard@users.noreply.github.com>
2022-05-01 17:30:38 +0800

    Add files via upload

---
+++ dataloaders/dataloader.py
@@ -0,0 +1,67 @@
+from __future__ import print_function, division
+import os
+from PIL import Image
+import numpy as np
+from torch.utils.data import Dataset
+from mypath import Path
+from glob import glob
+import random
+
+
+class Segmentation(Dataset):
+
+    def __init__(self,
+                 base_dir='/root/dataset',
+                 split='train',
+                 testid=None,
+                 transform=None
+                 ):
+        """
+        :param base_dir: path to VOC dataset directory
+        :param split: train/val
+        :param transform: transform to apply
+        """
+        # super().__init__()
+        self._base_dir = base_dir
+        self.image_list = []
+        self.split = split
+
+        self.image_pool = []
+        self.label_pool = []
+        self.img_name_pool = []
+        SEED = 1212
+        random.seed(SEED)
+
+        self._image_dir = os.path.join(self._base_dir,  split, 'image')
+        print(self._image_dir)
+        imagelist = glob(self._image_dir + "/*.png")
+        for image_path in imagelist:
+            gt_path = image_path.replace('image', 'mask')
+            self.image_list.append({'image': image_path, 'label': gt_path, 'id': testid})
+
+        self.transform = transform
+        # Display stats
+        print('Number of images in {}: {:d}'.format(split, len(self.image_list)))
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, index):
+        _img = Image.open(self.image_list[index]['image']).convert('RGB')
+        _target = Image.open(self.image_list[index]['label'])
+        if _target.mode is 'RGB':
+            _target=_target.convert('L')
+        _img_name = self.image_list[index]['image'].split('/')[-1]
+        anco_sample = {'image': _img, 'label': _target, 'img_name': _img_name}
+
+        if self.transform is not None:
+            anco_sample = self.transform(anco_sample)
+
+        return anco_sample
+
+
+
+    def __str__(self):
+        return 'Fundus(split=' + str(self.split) + ')'
+
+
---
+++ utils/metrics.py
@@ -0,0 +1,61 @@
+import torch
+
+import numpy as np
+
+
+class SegmentationMetric(object):
+    def __init__(self, numClass):
+        self.numClass = numClass
+        self.confusionMatrix = np.zeros((self.numClass,) * 2)
+
+    def pixelAccuracy(self):
+        acc = np.diag(self.confusionMatrix).sum() / self.confusionMatrix.sum()
+        return acc
+
+    def classPixelAccuracy(self):
+        classAcc = np.diag(self.confusionMatrix) / self.confusionMatrix.sum(axis=1)
+        return classAcc
+
+    def meanPixelAccuracy(self):
+        classAcc = self.classPixelAccuracy()
+        meanAcc = np.nanmean(classAcc)
+        return meanAcc
+
+    def IntersectionOverUnion(self):
+        intersection = np.diag(self.confusionMatrix)
+        union = np.sum(self.confusionMatrix, axis=1) + np.sum(self.confusionMatrix, axis=0) - np.diag(
+            self.confusionMatrix) 
+        IoU = intersection / union
+        return IoU
+    def recall(self):
+        # recall = TP / (TP + FN)
+        recall = np.diag(self.confusionMatrix) / self.confusionMatrix.sum(axis=0)
+        return recall
+    def meanIntersectionOverUnion(self):
+        mIoU = np.nanmean(self.IntersectionOverUnion())
+        return mIoU
+
+    def genConfusionMatrix(self, imgPredict, imgLabel):  #
+        # remove classes from unlabeled pixels in gt image and predict
+        mask = (imgLabel >= 0) & (imgLabel < self.numClass)
+        label = self.numClass * imgLabel[mask] + imgPredict[mask]
+        count = np.bincount(label, minlength=self.numClass ** 2)
+        confusionMatrix = count.reshape(self.numClass, self.numClass)
+        return confusionMatrix
+
+    def Frequency_Weighted_Intersection_over_Union(self):
+        freq = np.sum(self.confusion_matrix, axis=1) / np.sum(self.confusion_matrix)
+        iu = np.diag(self.confusion_matrix) / (
+                np.sum(self.confusion_matrix, axis=1) + np.sum(self.confusion_matrix, axis=0) -
+                np.diag(self.confusion_matrix))
+        FWIoU = (freq[freq > 0] * iu[freq > 0]).sum()
+        return FWIoU
+
+    def addBatch(self, imgPredict, imgLabel):
+        assert imgPredict.shape == imgLabel.shape
+        self.confusionMatrix += self.genConfusionMatrix(imgPredict, imgLabel)
+        return self.confusionMatrix
+
+    def reset(self):
+        self.confusionMatrix = np.zeros((self.numClass, self.numClass))
+    
