66356a5108e833104e1fe52a45da8b44967c26e7
ShanZard <87146029+ShanZard@users.noreply.github.com>
2022-05-01 17:31:19 +0800

    Delete mypath.py

--- mypath.py
+++
@@ -1,8 +0,0 @@
-class Path(object):
-    @staticmethod
-    def db_root_dir(database):
-        if database == 'fundus':
-            return '../../../../data/disc_cup_split/'  # foler that contains leftImg8bit/
-        else:
-            print('Database {} not available.'.format(database))
-            raise NotImplementedError
